42ef720fd299b5230846fbe71ab5a20c61f30e11
Juandi <Juandi@DESKTOP-LVHFGRV>
2021-09-29 23:47:02 +0200

    hecho

--- FormatoTexto/MainWindow.xaml.cs
+++ FormatoTexto/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace FormatoTexto
 {
@@ -24,5 +15,18 @@ namespace FormatoTexto
         {
             InitializeComponent();
         }
+
+        private void TextoACambiarTextBox_TextChanged(object sender, TextChangedEventArgs e) => TextoCambiadoTextBox.Text = TextoACambiarTextBox.Text;
+
+        private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontWeight = FontWeights.Bold;
+        private void NegritaCheckBox_UnChecked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontWeight = FontWeights.Normal;
+        private void CursivaCheckBox_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontStyle = FontStyles.Italic;
+        private void CursivaCheckBox_UnChecked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontStyle = FontStyles.Normal;
+        
+        private void AzulRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.Foreground = Brushes.Blue;
+
+        private void VerdeRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.Foreground = Brushes.Green;
+
+        private void RojoRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.Foreground = Brushes.Red;
     }
 }
