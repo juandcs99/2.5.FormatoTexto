758e0bc26f8f37c13f2527e4a50d61c275587969
Juandi <Juandi@DESKTOP-LVHFGRV>
2021-09-29 23:47:20 +0200

    hecho

--- FormatoTexto/MainWindow.xaml.cs
+++ FormatoTexto/MainWindow.xaml.cs
@@ -17,16 +17,12 @@ namespace FormatoTexto
         }
 
         private void TextoACambiarTextBox_TextChanged(object sender, TextChangedEventArgs e) => TextoCambiadoTextBox.Text = TextoACambiarTextBox.Text;
-
         private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontWeight = FontWeights.Bold;
         private void NegritaCheckBox_UnChecked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontWeight = FontWeights.Normal;
         private void CursivaCheckBox_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontStyle = FontStyles.Italic;
         private void CursivaCheckBox_UnChecked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontStyle = FontStyles.Normal;
-        
         private void AzulRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.Foreground = Brushes.Blue;
-
         private void VerdeRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.Foreground = Brushes.Green;
-
         private void RojoRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.Foreground = Brushes.Red;
     }
 }
