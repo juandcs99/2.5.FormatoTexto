16d4f06ae01c4b43e243b0cf57c0c136d4573173
Juan Diego <jd.cabrera@iesdoctorbalmis.com>
2021-09-30 13:53:46 +0200

    hecho

--- FormatoTexto/MainWindow.xaml.cs
+++ FormatoTexto/MainWindow.xaml.cs
@@ -16,13 +16,13 @@ namespace FormatoTexto
             InitializeComponent();
         }
 
-        private void TextoACambiarTextBox_TextChanged(object sender, TextChangedEventArgs e) => TextoCambiadoTextBox.Text = TextoACambiarTextBox.Text;
-        private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontWeight = FontWeights.Bold;
-        private void NegritaCheckBox_UnChecked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontWeight = FontWeights.Normal;
-        private void CursivaCheckBox_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontStyle = FontStyles.Italic;
-        private void CursivaCheckBox_UnChecked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.FontStyle = FontStyles.Normal;
-        private void AzulRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.Foreground = Brushes.Blue;
-        private void VerdeRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.Foreground = Brushes.Green;
-        private void RojoRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBox.Foreground = Brushes.Red;
+        private void TextoACambiarTextBox_TextChanged(object sender, TextChangedEventArgs e) => TextoCambiadoTextBlock.Text = TextoACambiarTextBox.Text;
+        private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBlock.FontWeight = FontWeights.Bold;
+        private void NegritaCheckBox_UnChecked(object sender, RoutedEventArgs e) => TextoCambiadoTextBlock.FontWeight = FontWeights.Normal;
+        private void CursivaCheckBox_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBlock.FontStyle = FontStyles.Italic;
+        private void CursivaCheckBox_UnChecked(object sender, RoutedEventArgs e) => TextoCambiadoTextBlock.FontStyle = FontStyles.Normal;
+        private void AzulRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBlock.Foreground = Brushes.Blue;
+        private void VerdeRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBlock.Foreground = Brushes.Green;
+        private void RojoRadioButton_Checked(object sender, RoutedEventArgs e) => TextoCambiadoTextBlock.Foreground = Brushes.Red;
     }
 }
